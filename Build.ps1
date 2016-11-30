3cfb560e292d18396d529e480470f6a294265e53
Luke M Griffith <lukemgriffith@gmail.com>
2016-11-30 20:44:19 +0000

    Created Build.ps1

--- Build.ps1
+++ Build.ps1
@@ -1,11 +1,14 @@
-function bar ($x){ Write-Host ($x * [Console]::WindowWidth) }
-function center ($x){ $a = " ".padleft(([Console]::WindowWidth / 2) - (($x -split "").count / 2)); Write-Host $a -NoNewline; Write-Host $x } 
-bar *
-bar *
-center "Lukes Build System."
+Set-Location $PSScriptRoot
+function bar ($x, $color = "White"){ Write-Host ($x * [Console]::WindowWidth) -ForegroundColor $color }
+function center ($x, $color = "White"){ $a = " ".padleft(([Console]::WindowWidth / 2) - (($x -split "").count / 2)); Write-Host $a -NoNewline -ForegroundColor $color; Write-Host $x -ForegroundColor $color } 
+
+bar * Green
+bar * Green
+center "Lukes Build System.`n`n"
+center $pwd
 
 
 switch ($args) {
 
-    {$_ -eq "Test"}  { Import-Module .\DynamicFunctions.Test -force; Invoke-Pester -CodeCoverage (Get-ChildItem -Recurse | ? {$_.Extension -in @(".psm1",".ps1")}) }
+    {$_ -eq "Test"}  { pushd; center "Starting Tests`n`n" Blue; Import-Module $psscriptroot\DynamicFunctions -force; Invoke-Pester -CodeCoverage (Get-ChildItem -Path .\DynamicFunctions -Recurse | ? {$_.Extension -in @(".psm1",".ps1")}).FullName; popd }
 }
--- DynamicFunctions.Test/DynamicFunctions.Test.psm1
+++ DynamicFunctions.Test/DynamicFunctions.Test.psm1
@@ -1,8 +1,5 @@
 
-# Potentailly move path to Module to an script, or evnrionment level variable. 
-Import-Module ..\DynamicFunctions -force
 
-Set-Location $PsScriptRoot
 
 
 
